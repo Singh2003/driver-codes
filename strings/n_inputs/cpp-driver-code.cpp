d5ea0cd1eb04814190a735a24d877407c5978440
vivek raj singh <77060093+Singh2003@users.noreply.github.com>
2022-04-22 12:59:18 +0530

    Update cpp-driver-code.cpp

--- strings/n_inputs/cpp-driver-code.cpp
+++ strings/n_inputs/cpp-driver-code.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
+#include<iomanip>
 
 #include<bits/stdc++.h>
 using namespace std;
 
 void solve (int opening_hh, int opening_mm, int duration_hh, int duration_mm) {
     // Your code goes here
+     int h;
+    int m;
+    h=opening_hh+duration_hh;
+    m=opening_mm+duration_mm;
+   
+    if(h>=24)
+        h=h-24;
+    if(m>=60)
+    {m=m-60;
+     h=h+1;
+    }
+    if(h>=24)
+    h=h-24;
+    
+    if(h<10)
+    h=00+h;
+    if(m<10)
+     m=00+m;
+    
+    std::cout<<setw(2)<<setfill('0')<<h<<"\t"<<setw(2)<<setfill('0')<<m;
+    
   
     // Print the output here
 }
